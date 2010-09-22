652d6e6db570e6dc60b2ea9d51a6fda34e4d99b1
David Murdoch <musicisair@yahoo.com>
2010-09-22 13:04:41 -0400

    Updating plugins.js to add Google Closure Compiler Comments and optimized closure, log and document.write methods.

--- js/plugins.js
+++ js/plugins.js
@@ -1,40 +1,65 @@
+/**
+* @preserve Plugins
+*/
+(
+/** 
+* Redefining "window" and "document" allows Google Closure 
+* Compiler to use single-character names for those 
+* variables. It also speeds lookups to those objects in most
+* browsers by localizing them to our closure.
+* @param {jQuery}  $ The jQuery object
+* @param {Window}  window The window
+* @param {Document}  document The document
+* @param {undefined=} undefined Makes sure undefined is undefined (optional)
+*/
+function ($, window, document, undefined) {
 
-// remap jQuery to $
-(function($){
-
- 
-
-
-
-
-
- 
-
-
-
-})(window.jQuery);
-
-
-
-// usage: log('inside coolFunc',this,arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
-};
+    /*  plugins */
 
 
 
-// catch all document.write() calls
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
 
 
+
+
+
+
+
+
+
+
+    /** 
+    * Usage: log('Inside coolFunc', this, arguments);
+    * http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+    * @param {...*} _args
+    */
+    var log = function (_args) {
+        log.history.push(arguments);
+        if (window.console) {
+            console.log(Array.prototype.slice.call(arguments));
+        }
+    };
+    log.history = [];
+
+    /**
+    * Closure around our new document.write()
+    * @param {function(*, ...[*])} write Native document.write
+    */
+    document.write = (function (write) {
+        var rwhiteList = /docwriteregexwhitelist/;
+        /**
+        * Catch all document.write() calls
+        * @param {...*} q The objects(s) being written
+        */
+        return function (q) {
+            log("document.write(): ", arguments);
+            if (rwhiteList.test(q)) {
+                write.apply(document, arguments);
+            }
+        };
+    } (document.write));
+
+    // Expose log globally.
+    window.log = log;
+
+} (jQuery, window, document));
