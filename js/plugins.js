990e5bdcfb229652383bf7efede989d144c4608b
Paul Irish <paul.irish@gmail.com>
2011-01-03 08:45:55 -0800

    cleaning up what's in plugins.js. no more closure wrap, no more document.write whitelisting.

--- js/plugins.js
+++ js/plugins.js
@@ -1,40 +1,13 @@
 
-// remap jQuery to $
-(function($){
-
- 
-
-
-
-
-
- 
-
-
-
-})(this.jQuery);
-
-
-
-// usage: log('inside coolFunc',this,arguments);
+// usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
 
 
 
-// catch all document.write() calls
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
-
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
 
