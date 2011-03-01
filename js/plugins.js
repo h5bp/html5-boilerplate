7a5705b57ad4db0737b5fbbb77b8bc7845c25884
Divya Manian <divya.manian@gmail.com>
2011-02-28 21:32:49 -0800

    window.log enhancement. thnx trevnorris. fixes #313

--- js/plugins.js
+++ js/plugins.js
@@ -4,6 +4,7 @@
 window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
+  arguments.callee = arguments.callee.caller;  
   if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
 
