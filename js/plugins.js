6d321ce898b22926ada3bfe1b8ea9fedef3b33cf
Paul Irish <paul.irish@gmail.com>
2011-03-07 00:10:07 -0800

    cleaning out all comments for the 1.0rc stripped.

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,9 @@
-
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
+  log.history = log.history || [];  
   log.history.push(arguments);
   arguments.callee = arguments.callee.caller;  
   if(this.console) console.log( Array.prototype.slice.call(arguments) );
 };
-// make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
 
