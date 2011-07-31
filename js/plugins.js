ebe10aef34b062ecc89dfbf18bd859a016dbbfed
Paul Irish <paul.irish@gmail.com>
2011-07-30 17:13:29 -0700

    improve the console.log nerfing for ff3.6. thx @drewcovi. fixes #455

--- js/plugins.js
+++ js/plugins.js
@@ -14,7 +14,8 @@ window.log = function(){
   }
 };
 // make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
 
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
