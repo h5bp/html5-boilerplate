8c724773f27fbc89fc513a9c28951cfb178fb460
Jeremey Hustman <jphustman@gmail.com>
2011-08-24 22:47:26 -0400

    remove old files

--- build/runbuildscript.bat
+++
@@ -1,5 +0,0 @@
-# This is for windows users only.
-# If you're on a mac or linux, just run `ant build` from this folder in Terminal
-
-set MYDIR=%~dp0
-ant build
--- js/plugins.js
+++
@@ -1,9 +0,0 @@
-window.log = function(){
-  log.history = log.history || [];
-  log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
-};
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
-
-
