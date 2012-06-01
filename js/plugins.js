7a2f9dcf8b56b6923a5bde028ea1838d32572588
Nicolas Gallagher <nicolasgallagher@gmail.com>
2012-06-01 22:23:31 +0100

    Formatting and consistency improvements
    - Consistent use of quotes in HTML.
    - Consistent use of hex for color values in CSS.
    - Multi-line CSS and JS.
    - 4 spaces for indentation.
    - Always use closing tags.
    - Use common DOCTYPE capitalization.
    
    These are steps toward reducing the number of repeat issues we get
    around inconsistencies or uncommon patterns; making diffs more readable;
    and using common conventions.

--- js/plugins.js
+++ js/plugins.js
@@ -1,11 +1,48 @@
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; try { args.callee = f.caller } catch(e) {}; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+
+window.log = function f() {
+    log.history = log.history || [];
+    log.history.push(arguments);
+    if (this.console) {
+        var args = arguments;
+        var newarr;
+
+        try {
+            args.callee = f.caller;
+        } catch(e) {
+
+        }
+
+        newarr = [].slice.call(args);
+
+        if (typeof console.log === 'object') {
+            log.apply.call(console.log, console, newarr);
+        } else {
+            console.log.apply(console, newarr);
+        }
+    }
+};
 
 // make it safe to use console.log always
-(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
-(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+
+(function(a) {
+    function b() {}
+    var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn";
+    var d;
+    for (c.split(","); !!(d = c.pop());) {
+        a[d] = a[d] || b;
+    }
+});
+
+(function() {
+    try {
+        console.log();
+        return window.console;
+    } catch(a) {
+        return (window.console = {});
+    }
+}());
 
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
-
