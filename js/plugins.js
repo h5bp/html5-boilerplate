e6c64a238d5438dbc28ea09a115ee44cb6f144ad
Nicolas Gallagher <nicolasgallagher@gmail.com>
2012-09-19 23:28:55 +0100

    Remove duplicate `markTimeline` from plugins.js
    Fix gh-1211

--- js/plugins.js
+++ js/plugins.js
@@ -2,7 +2,7 @@
 if (!(window.console && console.log)) {
     (function() {
         var noop = function() {};
-        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
         var length = methods.length;
         var console = window.console = {};
         while (length--) {
