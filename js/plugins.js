3fd52ae05a3feb951acc8cad6f8a73a0e5511d65
tangxiaoming <tangxm90@gmail.com>
2012-09-28 14:03:17 +0800

    Update js/plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,12 +1,10 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var noop = function noop() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
+    var methods = 'assert, clear, count, debug, dir, dirxml, error,\
+        exception, group, groupCollapsed, groupEnd, info, log, markTimeline,\
+        profile, profileEnd, table, time, timeEnd, timeStamp, trace, warn'.split(',');
+        
     var length = methods.length;
     var console = window.console || {};
 
