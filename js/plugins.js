195caccd544e890df982255f347d03866002a0d4
Will Morgan <me@willmorgan.co.uk>
2012-10-07 20:28:08 +0200

    Merging all variables back to var

--- js/plugins.js
+++ js/plugins.js
@@ -1,14 +1,14 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var noop = function noop() {};
-    var methods = [
+    var noop = function noop() {},
+        methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
         'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
         'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = window.console || {};
+        ],
+        length = methods.length,
+        console = window.console || {};
 
     while (length--) {
         // Only stub undefined methods.
