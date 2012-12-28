a2b3d06e8b5b0bfb3bd608a1b938c9fa50b1c600
Devin Rhode <devinrhode2@gmail.com>
2012-12-28 01:17:17 -0600

    Remove named function expression in plugins.js
    Details on various bugs with named function expressions:
    http://kangax.github.com/nfe/#jscript-bugs

--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
-    var noop = function noop() {};
+    var noop = function () {};
     var methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
