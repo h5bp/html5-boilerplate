3b6d5df860b8a950a299e77a702a4e8921dc9252
Andrew George <IT@Andrews-MacBook-Pro.local>
2013-03-22 16:44:39 -0700

    Adding new images update the css, index and js to do horizontal movement too.

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 	// Cache the Y offset and the speed of each sprite
 	$('[data-type]').each(function() {	
 		$(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
-		$(this).data('Xposition', $(this).attr('data-Xposition'));
+		$(this).data('Xposition', parseInt($(this).attr('data-Xposition')));
 		$(this).data('speed', $(this).attr('data-speed'));
 	});
 	
@@ -46,11 +46,14 @@ $(document).ready(function(){
 					yPos += $self.data('offsetY');
 				}
 				
+				
 				// Put together our final background position
 				var coords = '50% '+ yPos + 'px';
 
 				// Move the background
 				$self.css({ backgroundPosition: coords });
+				
+				$('.data').append('<p>'+$window.scrollTop()+'</p>');
 				
 				// Check for other sprites in this section	
 				$('[data-type="sprite"]', $self).each(function() {
@@ -60,11 +63,28 @@ $(document).ready(function(){
 					
 					// Use the same calculation to work out how far to scroll the sprite
 					var yPos = -($window.scrollTop() / $sprite.data('speed'));					
-					var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
-					
+					var coords = $sprite.data('Xposition') + '% ' + (yPos + $sprite.data('offsetY')) + 'px';
 					$sprite.css({ backgroundPosition: coords });													
 					
 				}); // sprites
+				
+				// Check for other sprites in this section	
+				$('[data-type="spriteQuad"]', $self).each(function() {
+					
+					// Cache the sprite
+					var $sprite = $(this);
+					
+					// Use the same calculation to work out how far to scroll the sprite
+					var yPos = ($window.scrollTop() / $sprite.data('speed'));
+					var xPos = ($sprite.data('Xposition') + ($window.scrollTop() / 0.25));
+									
+					var coords = xPos + 'px ' + (yPos + $sprite.data('offsetY')) + 'px';
+					$sprite.css({ backgroundPosition: coords });
+					$sprite.append($window.scrollTop());													
+					
+				}); // sprites
+				
+				
 			
 				// Check for any Videos that need scrolling
 				$('[data-type="video"]', $self).each(function() {
