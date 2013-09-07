27b174bf22ef4430f1b8930ebc858cd0702e9479
Pere Orga <gotrunks@gmail.com>
2013-09-07 20:04:43 +0100

    Add link to http://rmurphey.com/blog/2009/10/15/using-objects-to-organize-your-code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// For a better and harder way of organizing your Javascript code, see
+// http://rmurphey.com/blog/2009/10/15/using-objects-to-organize-your-code/
+
 (function($) {
 
     // jQuery code using the $() function.
