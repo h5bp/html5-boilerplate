14935f09e9472db855b18859aa04d7858b439074
Ram kumar <ramtml@users.noreply.github.com>
2015-05-29 14:51:41 +0530

    Update plugins.js

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,24 +1,38 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+;
+(function ($, window, document, undefined) {
+ 
+    var defaults = {
+        color: "red" //rename your option
+    };
+ 
+    function Plugin(element, options) {
+ 
+        this.el = element;
 
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+        this.pluginName;
+        
+        this.options = $.extend({}, defaults, options);
+        
+        this.init();
+        
+        return this;
     }
-}());
+ 
+    Plugin.prototype.init = function () {
+        $(this.el).css('color', this.options.color);//setting up your option
+    };
+ 
+    $.fn.pluginName = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_')) {
+                $.data(this, 'plugin_', new Plugin(this, options));
+            }
+        });
+    };
+ 
+})(jQuery, window, document);
+
+
 
 // Place any jQuery/helper plugins in here.
