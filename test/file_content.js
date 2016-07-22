5be7cd184fa682c89cdbc871ad623b2a42a80f55
Kirill <seokirillstarsnab@gmail.com>
2016-07-22 10:04:45 +0300

    no message

--- test/file_content.js
+++ test/file_content.js
@@ -56,24 +56,7 @@ function runTests() {
             var string = '\n\nErrorDocument 404 /404.html\n\n';
             checkString(path.resolve(dir, '.htaccess'), string, done);
         });
-
-        it('"index.html" should contain the correct jQuery version in the CDN URL', function (done) {
-            var string = 'code.jquery.com/jquery-' + pkg.devDependencies.jquery + '.min.js';
-            checkString(path.resolve(dir, 'index.html'), string, done);
-        });
-
-        it('"index.html" should contain the correct jQuery version in the local URL', function (done) {
-            var string = 'js/vendor/jquery-' + pkg.devDependencies.jquery + '.min.js';
-            checkString(path.resolve(dir, 'index.html'), string, done);
-        });
-
-        it('"main.css" should contain a custom banner', function (done) {
-            var string = '/*! HTML5 Boilerplate v' + pkg.version +
-                         ' | ' + pkg.license.type + ' License' +
-                         ' | ' + pkg.homepage + ' */\n\n/*\n';
-            checkString(path.resolve(dir, 'css/main.css'), string, done);
-        });
-
+        
     });
 
 }
