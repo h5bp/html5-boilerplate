f121b530232ceb9c916bdac0b2e0605aa7839d97
Rob Larsen <rob@drunkenfist.com>
2018-04-06 17:31:27 -0400

    Features/eslint (#2037)
    Fixes #1913
    
    * Drops node 4
    
    * update node engine to >=6 (#2035)
    
    * syncs changes to spacing across different editorconfigs
    
    * adds eslint with minimally invasive configuration- basically moving from 4 to 2 spaces.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -28,149 +28,148 @@ const dirs = pkg['h5bp-configs'].directories;
 // ---------------------------------------------------------------------
 
 gulp.task('archive:create_archive_dir', () => {
-    fs.mkdirSync(path.resolve(dirs.archive), '0755');
+  fs.mkdirSync(path.resolve(dirs.archive), '0755');
 });
 
 gulp.task('archive:zip', (done) => {
 
-    const archiveName = path.resolve(dirs.archive, `${pkg.name}_v${pkg.version}.zip`);
-    const zip = archiver('zip');
-    const files = glob.sync('**/*.*', {
-        'cwd': dirs.dist,
-        'dot': true // include hidden files
-    });
-    const output = fs.createWriteStream(archiveName);
+  const archiveName = path.resolve(dirs.archive, `${pkg.name}_v${pkg.version}.zip`);
+  const zip = archiver('zip');
+  const files = glob.sync('**/*.*', {
+    'cwd': dirs.dist,
+    'dot': true // include hidden files
+  });
+  const output = fs.createWriteStream(archiveName);
 
-    zip.on('error', (error) => {
-        done();
-        throw error;
-    });
+  zip.on('error', (error) => {
+    done();
+    throw error;
+  });
 
-    output.on('close', done);
+  output.on('close', done);
 
-    files.forEach( (file) => {
+  files.forEach((file) => {
 
-        const filePath = path.resolve(dirs.dist, file);
+    const filePath = path.resolve(dirs.dist, file);
 
-        // `zip.bulk` does not maintain the file
-        // permissions, so we need to add files individually
-        zip.append(fs.createReadStream(filePath), {
-            'name': file,
-            'mode': fs.statSync(filePath).mode
-        });
-
+    // `zip.bulk` does not maintain the file
+    // permissions, so we need to add files individually
+    zip.append(fs.createReadStream(filePath), {
+      'name': file,
+      'mode': fs.statSync(filePath).mode
     });
 
-    zip.pipe(output);
-    zip.finalize();
+  });
+
+  zip.pipe(output);
+  zip.finalize();
 
 });
 
 gulp.task('clean', (done) => {
-    del([
-        dirs.archive,
-        dirs.dist
-    ]).then( () => {
-        done();
-    });
+  del([
+    dirs.archive,
+    dirs.dist
+  ]).then(() => {
+    done();
+  });
 });
 
 gulp.task('copy', [
-    'copy:.htaccess',
-    'copy:index.html',
-    'copy:jquery',
-    'copy:license',
-    'copy:main.css',
-    'copy:misc',
-    'copy:normalize'
+  'copy:.htaccess',
+  'copy:index.html',
+  'copy:jquery',
+  'copy:license',
+  'copy:main.css',
+  'copy:misc',
+  'copy:normalize'
 ]);
 
 gulp.task('copy:.htaccess', () =>
-    gulp.src('node_modules/apache-server-configs/dist/.htaccess')
-        .pipe(plugins().replace(/# ErrorDocument/g, 'ErrorDocument'))
-        .pipe(gulp.dest(dirs.dist))
+  gulp.src('node_modules/apache-server-configs/dist/.htaccess')
+    .pipe(plugins().replace(/# ErrorDocument/g, 'ErrorDocument'))
+    .pipe(gulp.dest(dirs.dist))
 );
 
 gulp.task('copy:index.html', () => {
-    const hash = ssri.fromData(
-        fs.readFileSync('node_modules/jquery/dist/jquery.min.js'),
-        { algorithms: ['sha256'] }
-    );
-    let version = pkg.devDependencies.jquery;
-    let modernizrVersion = pkg.devDependencies.modernizr;
+  const hash = ssri.fromData(
+    fs.readFileSync('node_modules/jquery/dist/jquery.min.js'),
+    {algorithms: ['sha256']}
+  );
+  let version = pkg.devDependencies.jquery;
+  let modernizrVersion = pkg.devDependencies.modernizr;
 
-    gulp.src(`${dirs.src}/index.html`)
-        .pipe(plugins().replace(/{{JQUERY_VERSION}}/g, version))
-        .pipe(plugins().replace(/{{MODERNIZR_VERSION}}/g, modernizrVersion))
-        .pipe(plugins().replace(/{{JQUERY_SRI_HASH}}/g, hash.toString()))
-        .pipe(gulp.dest(dirs.dist));
+  gulp.src(`${dirs.src}/index.html`)
+    .pipe(plugins().replace(/{{JQUERY_VERSION}}/g, version))
+    .pipe(plugins().replace(/{{MODERNIZR_VERSION}}/g, modernizrVersion))
+    .pipe(plugins().replace(/{{JQUERY_SRI_HASH}}/g, hash.toString()))
+    .pipe(gulp.dest(dirs.dist));
 });
 
 gulp.task('copy:jquery', () =>
-    gulp.src(['node_modules/jquery/dist/jquery.min.js'])
-        .pipe(plugins().rename(`jquery-${pkg.devDependencies.jquery}.min.js`))
-        .pipe(gulp.dest(`${dirs.dist}/js/vendor`))
+  gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+    .pipe(plugins().rename(`jquery-${pkg.devDependencies.jquery}.min.js`))
+    .pipe(gulp.dest(`${dirs.dist}/js/vendor`))
 );
 
 gulp.task('copy:license', () =>
-    gulp.src('LICENSE.txt')
-        .pipe(gulp.dest(dirs.dist))
+  gulp.src('LICENSE.txt')
+    .pipe(gulp.dest(dirs.dist))
 );
 
 gulp.task('copy:main.css', () => {
 
-    const banner = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n\n`;
+  const banner = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n\n`;
 
-    gulp.src(`${dirs.src}/css/main.css`)
-        .pipe(plugins().header(banner))
-        .pipe(plugins().autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9', '> 1%'],
-            cascade: false
-        }))
-        .pipe(gulp.dest(`${dirs.dist}/css`));
+  gulp.src(`${dirs.src}/css/main.css`)
+    .pipe(plugins().header(banner))
+    .pipe(plugins().autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9', '> 1%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(`${dirs.dist}/css`));
 });
 
 gulp.task('copy:misc', () =>
-    gulp.src([
+  gulp.src([
 
-        // Copy all files
-        `${dirs.src}/**/*`,
+    // Copy all files
+    `${dirs.src}/**/*`,
 
-        // Exclude the following files
-        // (other tasks will handle the copying of these files)
-        `!${dirs.src}/css/main.css`,
-        `!${dirs.src}/index.html`
+    // Exclude the following files
+    // (other tasks will handle the copying of these files)
+    `!${dirs.src}/css/main.css`,
+    `!${dirs.src}/index.html`
 
-    ], {
+  ], {
 
-        // Include hidden files by default
-        dot: true
+    // Include hidden files by default
+    dot: true
 
-    }).pipe(gulp.dest(dirs.dist))
+  }).pipe(gulp.dest(dirs.dist))
 );
 
 gulp.task('copy:normalize', () =>
-    gulp.src('node_modules/normalize.css/normalize.css')
-        .pipe(gulp.dest(`${dirs.dist}/css`))
+  gulp.src('node_modules/normalize.css/normalize.css')
+    .pipe(gulp.dest(`${dirs.dist}/css`))
 );
 
-gulp.task( 'modernizr', (done) =>{
+gulp.task('modernizr', (done) =>{
 
-    modernizr.build(modernizrConfig, (code) => {
-        fs.writeFile(`${dirs.dist}/js/vendor/modernizr-${pkg.devDependencies.modernizr}.min.js`, code, done);
-    });
+  modernizr.build(modernizrConfig, (code) => {
+    fs.writeFile(`${dirs.dist}/js/vendor/modernizr-${pkg.devDependencies.modernizr}.min.js`, code, done);
+  });
 
 });
 
 gulp.task('lint:js', () =>
-    gulp.src([
-        'gulpfile.js',
-        `${dirs.src}/js/*.js`,
-        `${dirs.test}/*.js`
-    ]).pipe(plugins().jscs())
-      .pipe(plugins().jshint())
-      .pipe(plugins().jshint.reporter('jshint-stylish'))
-      .pipe(plugins().jshint.reporter('fail'))
+  gulp.src([
+    'gulpfile.js',
+    `${dirs.src}/js/*.js`,
+    `${dirs.test}/*.js`
+  ]).pipe(plugins().jscs())
+    .pipe(plugins().eslint())
+    .pipe(plugins().eslint.failOnError())
 );
 
 
@@ -179,17 +178,17 @@ gulp.task('lint:js', () =>
 // ---------------------------------------------------------------------
 
 gulp.task('archive', (done) => {
-    runSequence(
-        'build',
-        'archive:create_archive_dir',
-        'archive:zip',
+  runSequence(
+    'build',
+    'archive:create_archive_dir',
+    'archive:zip',
     done);
 });
 
 gulp.task('build', (done) => {
-    runSequence(
-        ['clean', 'lint:js'],
-        'copy', 'modernizr',
+  runSequence(
+    ['clean', 'lint:js'],
+    'copy', 'modernizr',
     done);
 });
 
