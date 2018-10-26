0fcef067b43e0c08d7bdbc8e6987500c6ee79493
Mark Williams <mark.williams027@gmail.com>
2018-10-26 12:00:55 -0400

    Gulp
    Added Gulp and package.json

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+    return gulp.src('css/scss/style.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    })
+})
+
+gulp.task('default', ['browserSync', 'sass'], function(){
+    gulp.watch('css/scss/**/*.scss', ['sass']); 
+    gulp.watch('./*.html', browserSync.reload); 
+    gulp.watch('./js/**/*.js', browserSync.reload);
+})
