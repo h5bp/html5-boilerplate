c79799382f32936aea8350d2124b9c3dea3b9a9b
Christian Oliff <christianoliff@yahoo.com>
2019-10-24 12:00:40 +0900

    Change indenting to 2 spaces to match editorconfig code guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "mocha": true
-    },
-    "plugins": ["mocha"],
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true
+  },
+  plugins: ["mocha"],
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
 };
