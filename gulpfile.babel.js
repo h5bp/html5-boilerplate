5d3e0c4350bd24f2c7318946e017b0c4a3f20020
Rob Larsen <rob@htmlcssjavascript.com>
2020-04-28 11:18:55 -0400

    Simplified dir creation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -120,9 +120,7 @@ gulp.task('copy:normalize', () =>
 
 gulp.task('modernizr', (done) => {
   modernizr.build(modernizrConfig, (code) => {
-    if (!fs.existsSync(`${dirs.dist}/js/vendor/`)) {
-      fs.mkdirSync(`${dirs.dist}/js/vendor/`);
-    }
+    fs.mkdir(`${dirs.dist}/js/vendor/`);
     fs.writeFile(`${dirs.dist}/js/vendor/modernizr-${pkg.devDependencies.modernizr}.min.js`, code, done);
   });
 });
