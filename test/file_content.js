a8bf689e9b3f71e83b5c57d33f4733e6e751aa38
sthiepaan <info@damianszczypka.com>
2020-09-27 10:21:29 +0200

    Fix test against CSS file

--- test/file_content.js
+++ test/file_content.js
@@ -61,9 +61,9 @@ function runTests() {
       checkString(path.resolve(dir, 'index.html'), string, done);
     });
 
-    it('"main.css" should contain a custom banner', function (done) {
+    it('"style.css" should contain a custom banner', function (done) {
       const string = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n`;
-      checkString(path.resolve(dir, 'css/main.css'), string, done);
+      checkString(path.resolve(dir, 'css/style.css'), string, done);
     });
 
   });
