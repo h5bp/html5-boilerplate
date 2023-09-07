29d7d8054b82401274608c856b70e11de700be6f
Christian Oliff <christianoliff@pm.me>
2023-09-08 05:20:47 +0900

    Add Prettier (#3011)
    Consistent code formatting!
    (it fixed a few minor bugs too)

--- test/file_content.mjs
+++ test/file_content.mjs
@@ -9,21 +9,17 @@ const dirs = pkg['h5bp-configs'].directories;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function checkString(file, string, done) {
-
   let character = '';
   let matchFound = false;
   let matchedPositions = 0;
-  const readStream = fs.createReadStream(file, {'encoding': 'utf8'});
+  const readStream = fs.createReadStream(file, { encoding: 'utf8' });
 
   readStream.on('close', done);
   readStream.on('error', done);
   readStream.on('readable', function () {
-
     // Read file until the string is found
     // or the whole file has been read
-    while (matchFound !== true &&
-                (character = readStream.read(1)) !== null) {
-
+    while (matchFound !== true && (character = readStream.read(1)) !== null) {
       if (character === string.charAt(matchedPositions)) {
         matchedPositions += 1;
       } else {
@@ -33,31 +29,24 @@ function checkString(file, string, done) {
       if (matchedPositions === string.length) {
         matchFound = true;
       }
-
     }
 
     assert.equal(true, matchFound);
     this.close();
-
   });
-
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function runTests() {
-
   const dir = dirs.dist;
 
   describe(`Test if the files from the "${dir}" directory have the expected content`, () => {
-
     it('"style.css" should contain a custom banner', function (done) {
       const string = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n`;
       checkString(path.resolve(dir, 'css/style.css'), string, done);
     });
-
   });
-
 }
 
 runTests();
