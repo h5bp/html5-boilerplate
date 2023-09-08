f2a363bf1c21417e4dd6a836ff689646a78efaef
Rob Larsen <rob@htmlcssjavascript.com>
2023-09-08 18:45:29 +0000

    Removing gulp replace

--- dist/webpack.config.dev.js
+++ dist/webpack.config.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
@@ -12,5 +11,3 @@ module.exports = merge(common, {
     static: ['./'],
   },
 });
-
-
