a0a80e460290a717f45a25c83b0dd8a2cc16c4a5
alaaraafeh <alaaraafeh@gmail.com>
2025-12-08 19:17:31 +0200

    modfiy js insert new html index file

--- src/webpack.config.dev.js
+++ src/webpack.config.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+// this is just test
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
